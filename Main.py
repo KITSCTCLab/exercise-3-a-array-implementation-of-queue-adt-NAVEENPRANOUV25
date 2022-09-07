71b0020cd04141deadbaaabe9eaab795d945a308
NAVEENPRANOUV25 <109648415+NAVEENPRANOUV25@users.noreply.github.com>
2022-09-07 10:11:26 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -10,7 +10,7 @@ class Solution:
       """
 
     # Write your code here
-    def __init__(self, size):
+    def _init_(self, size):
         """Inits Solution with stack, queue, size, top, front and rear.
         Arguments:
           size: An integer to set the size of stack and queue.
@@ -71,7 +71,7 @@ class Solution:
             character: A character that will be enqueued to queue.
         """
         if not self.is_queue_full():
-            if  self.front == -1:
+            if self.front == -1:
                 self.front = 0
             self.rear += 1
             self.queue.append(character)
@@ -129,87 +129,3 @@ if is_palindrome:
     print("The word, " + text + ", is a palindrome.")
 else:
     print("The word, " + text + ", is not a palindrome.")
-class MyCircularQueue:
-    def _init_(self, size: int):
-        self.size=size
-        self.queue=[None]*size
-        self.rear=-1
-        self.front=-1
-
-    def enqueue(self, value: int) -> bool:
-       
-        if(self.is_full()==False):
-            if(self.front==-1):
-                self.front=0
-                self.rear=0
-                self.queue[self.rear]=value
-            else:
-                self.rear=(self.rear+1)%self.size
-                self.queue[self.rear]=value
-            return True
-        else:
-            return False
-
-    def dequeue(self) -> bool:
-        if(self.is_empty()==False):
-            if(self.front==self.rear):
-                self.front=-1
-                self.rear=-1
-                return True
-            else:
-                self.front=(self.front+1)%self.size
-                return True
-        else:
-            return False
-               
-
-    def get_front(self) -> int:
-        if(self.is_empty()==False):
-            return self.queue[self.front]
-        else:
-            return -1
-
-    def get_rear(self):
-        if(self.is_empty()==False):
-            return self.queue[self.rear]
-        else:
-            return -1
-
-    def is_empty(self):
-        return self.front==-1
-
-    def is_full(self):
-        return (self.rear+1)%self.size==self.front
-           
-
-
-# Do not change the following code
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-data = []
-for item in input().split(','):
-    item = item.strip()
-    if item == '-':
-        data.append([])
-    else:
-        data.append([int(item)])
-obj = MyCircularQueue(data[0][0])
-result = []
-for i in range(len(operations)):
-    if i == 0:
-        result.append(None)
-    elif operations[i] == "enqueue":
-        result.append(obj.enqueue(data[i][0]))
-    elif operations[i] == "get_rear":
-        result.append(obj.get_rear())
-    elif operations[i] == "get_front":
-        result.append(obj.get_front())
-    elif operations[i] == "dequeue":
-        result.append(obj.dequeue())
-    elif operations[i] == "is_full":
-        result.append(obj.is_full())
-    elif operations[i] == "is_empty":
-        result.append(obj.is_empty())
-
-print(result)
